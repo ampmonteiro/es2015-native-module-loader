fdae1afaad45d4c331e2f9e78c36f805c338174f
ampmonteiro <ampm@elCapitan.local>
2017-10-26 16:03:42 +0100

    update mainjs file with some code to oldCalc branch

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-// main file
-dasd
+// main file to run Old way with OldCalc
+
+// selection the tag elements
+
+var sum = document.querySelector("button#soma");
+var mult = document.querySelector("button#multi");
+var res = document.querySelector("div#res");
+
+
+
+// the event listeners of two buttons to do the operations
+
+sum.addEventListener("click", 
+                      function(){
+
+
+
+});
+
+mult.addEventListener("click", 
+                      function(){
+
+
+
+});
+
