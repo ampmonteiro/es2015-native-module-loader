f2378be38c26800ba30bff5000e623df557e00de
ampmonteiro <>
2018-01-13 22:54:27 +0000

    return methods with short way

--- oldCalc.js
+++ oldCalc.js
@@ -3,22 +3,29 @@
 var oldCalc = (function () {
 
   // Keep this variable and Methods private inside this closure scope
-    
+
     var sum = function(a,b) {
-      
+
       return a+b;
     };
-  
+
     var mult = function(a,b) {
       return a*b;
     };
 
-  // Explicitly reveal public pointers to the private functions 
+  // Explicitly reveal public pointers to the private functions
   // that we want to reveal publicly
 
   return {
     sum: sum,
     mult: mult
   }
-  
+
+  // or short way
+
+  // return {
+  //   sum,
+  //   mult
+  // }
+
 })();
